98c5b3801ef5a3ab7883ab1a3a00b844d76e63b7
LETI_111270 <mfhpo@iscte-iul.pt>
2025-10-21 00:44:20 +0100

    Fixed the currency exchange

--- src/main/java/com/example/moneyconvertion/CurrencyExchange.java
+++ src/main/java/com/example/moneyconvertion/CurrencyExchange.java
@@ -1,14 +1,21 @@
 package com.example.moneyconvertion;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Instant;
-import javax.money.convert.*;
-import org.javamoney.moneta.Money;
-import javax.money.*;
+import org.joda.money.*;
+import java.net.*;
+import java.io.*;
 
 @Entity
 @Table(name = "currency_exchange")
 public class CurrencyExchange {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -30,47 +37,80 @@ public class CurrencyExchange {
     @Column(name = "exchange_date", nullable = false)
     private Instant exchangeDate;
 
-    protected CurrencyExchange() {}
 
-    public CurrencyExchange(String fromCurrency, String toCurrency, double amount, double result, Instant exchangeDate) {
+
+    protected CurrencyExchange() {}// To keep Hibernate happy
+
+    public CurrencyExchange(String fromCurrency, String toCurrency, double amount, Instant exchangeDate) {
         this.fromCurrency = fromCurrency;
         this.toCurrency = toCurrency;
         this.amount = amount;
-
-        MonetaryAmount number = Money.of(50, fromCurrency);
-
-        CurrencyConversion toNewCurrency = MonetaryConversions.getConversion(toCurrency);
-        MonetaryAmount converted = number.with(toNewCurrency);
-        result = converted.getNumber().doubleValue();
-
-        System.out.println("Converted " + number + " to " + result);
-
-
-
-        this.result = result;
         this.exchangeDate = exchangeDate;
+
+        CurrencyUnit fromCurrencyUnit = CurrencyUnit.of(fromCurrency);
+        CurrencyUnit toCurrencyUnit = CurrencyUnit.of(toCurrency);
+        Money fromMoney = Money.of(fromCurrencyUnit, amount);
+        CurrencyUnit usd = CurrencyUnit.of("USD");
+        if (fromCurrency.equals(toCurrency)) {
+            this.result = amount;
+            return;
+        }
+        try {
+            JsonObject exchangeRates = getExchangeRates(fromCurrency);
+            JsonObject conversion_rates = exchangeRates.getAsJsonObject("conversion_rates");
+            double toRate = conversion_rates.get(toCurrency).getAsDouble();
+            Money toMoney = fromMoney.convertedTo(toCurrencyUnit, BigDecimal.valueOf(toRate), RoundingMode.HALF_UP);
+            this.result = toMoney.getAmount().doubleValue();
+        } catch (IOException e) {
+            e.printStackTrace();
+            this.result = 0.0;
+        }
+
+
+
+    }
+
+    private JsonObject getExchangeRates(String currency) throws IOException {
+        // Setting URL
+        String url_str = "https://v6.exchangerate-api.com/v6/458fc04a58b0e5feff7f1aa0/latest/" + currency;
+
+        // Making Request
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        // Convert to JSON
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+        // Accessing object
+        String req_result = jsonobj.get("result").getAsString();
+        System.out.println("Request Result: " + req_result);
+        return jsonobj;
     }
 
 
+
+    public Long getId() {
+        return id;
+    }
     public String getFromCurrency() {
         return fromCurrency;
     }
-
-    public String getToCurrency() {
-        return toCurrency;
-    }
-
     public double getAmount() {
         return amount;
+    }
+
+    public Object getToCurrency() {
+        return toCurrency;
     }
 
     public double getResult() {
         return result;
     }
-
     public Instant getExchangeDate() {
         return exchangeDate;
     }
-
 }
 
--- src/main/java/com/example/moneyconvertion/CurrencyExchangeService.java
+++ src/main/java/com/example/moneyconvertion/CurrencyExchangeService.java
@@ -1,12 +1,6 @@
 package com.example.moneyconvertion;
 
-import org.javamoney.moneta.Money;
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import javax.money.MonetaryAmount;
-import javax.money.convert.ConversionQueryBuilder;
-import javax.money.convert.CurrencyConversion;
-import javax.money.convert.MonetaryConversions;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,16 +18,8 @@ public class CurrencyExchangeService {
 
     @Transactional
     public CurrencyExchange convertAndSave(String from, String to, double amount) {
-        CurrencyUnit fromUnit = Monetary.getCurrency(from);
-        CurrencyUnit toUnit = Monetary.getCurrency(to);
-        MonetaryAmount fromAmount = Money.of(amount, fromUnit);
-        CurrencyConversion conversion = MonetaryConversions.getConversion(
-                ConversionQueryBuilder.of().setTermCurrency(toUnit).build()
-        );
-        MonetaryAmount toAmount = fromAmount.with(conversion);
-        double result = toAmount.getNumber().doubleValue();
 
-        CurrencyExchange exchange = new CurrencyExchange(from, to, amount, result, Instant.now());
+        CurrencyExchange exchange = new CurrencyExchange(from, to, amount, Instant.now());
         return repository.saveAndFlush(exchange);
     }
 
